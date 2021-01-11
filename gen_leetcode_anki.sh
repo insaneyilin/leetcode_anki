64e585c943a4d70db2e7560b7a932dc42fadbf25
guiyilin <yilin.gui@gmail.com>
2021-01-11 20:12:03 +0800

    update gen_leetcode_anki.sh, use zsh

--- gen_leetcode_anki.sh
+++ gen_leetcode_anki.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 rm -f leetcode_mdanki.apkg
 mkdir -p temp
-ls **/lc_*.md | xargs -I file cp file temp/
+ls **/*.md | xargs -I file cp file temp/
+rm -f temp/README.md
 echo "generating leetcode_mdanki.apkg ..."
 mdanki temp leetcode_mdanki.apkg
 rm -r temp
